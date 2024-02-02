7b1c0d1c1f6a636a777427684bc814cd4861a183
Nakamura <80927415+gustavonakamura@users.noreply.github.com>
2024-02-02 11:19:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 <script type="text/javascript">
   function formatarMoeda() {
-    var elemento = document.getElementById('ValorPrestacaoServico');
-    var valor = elemento.value;
+        var elemento = document.getElementById('ValorPrestacaoServico');
+        var valor = elemento.value;
 
-    valor = valor + '';
-    valor = parseInt(valor.replace(/[\D]+/g, ''));
-    valor = valor + '';
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
+        valor = valor + '';
+        valor = parseInt(valor.replace(/[\D]+/g, ''));
+        valor = valor + '';
+        valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
+        if (valor.length > 6) {
+            valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+        }
 
-// Serve para o label não fica vazio
-    if (!isNaN(parseFloat(valor))) {
+        if (isNaN(parseFloat(valor.replace(',', '.'))) || valor.trim() === '') {
+            return;
+        }
         elemento.value = valor;
-    } else {
-        elemento.value = '';
     }
-}
 </scritp>
