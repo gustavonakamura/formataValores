e9134e36bc8a6d749b3c7b87600ce41196b20a37
Nakamura <80927415+gustavonakamura@users.noreply.github.com>
2024-02-05 08:44:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 <script type="text/javascript">
   function formatarMoeda() {
-        var elemento = document.getElementById('ValorPrestacaoServico');
+        var elemento = document.getElementById('valorPrestacaoServico');
         var valor = elemento.value;
 
-        valor = valor + '';
-        valor = parseInt(valor.replace(/[\D]+/g, ''));
-        valor = valor + '';
+        // Remove caracteres não numéricos
+        valor = valor.replace(/\D/g, '');
+
+        // Adiciona vírgula para separar os centavos
         valor = valor.replace(/([0-9]{2})$/g, ",$1");
 
+        // Adiciona ponto para separar milhares
         if (valor.length > 6) {
             valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
         }
 
-        if (isNaN(parseFloat(valor.replace(',', '.'))) || valor.trim() === '') {
-            return;
-        }
+        // Atualiza o valor no campo
         elemento.value = valor;
-    }
+    }  
 </scritp>
